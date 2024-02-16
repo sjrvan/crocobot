918e5d2ab799339cd6b250c3a499e07b172dd31c
sjrvan <101860064+sjrvan@users.noreply.github.com>
2024-02-16 13:55:08 +0400

    Update misc.py

--- misc.py
+++ misc.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from game import Game
 
 
-bot = Bot(token=os.getenv('CROCOBOT_TOKEN'))
+bot = Bot(token=os.getenv('6328093163:AAHOKzMvaEHs6znJxCFA9IYaWypdkbnYK-4'))
 dp = Dispatcher(bot)
 g = Game('crocobot.db')
 logging.basicConfig(level=logging.INFO)
